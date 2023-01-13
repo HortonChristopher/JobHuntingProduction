37ab9ce16636d64875898ff1dba543903a6ee757
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2023-01-13 23:49:35 +0900

    Added basics to loading files

--- Pursuer/scene/Loading.cpp
+++ Pursuer/scene/Loading.cpp
@@ -1,1 +1,105 @@
 #include "Loading.h"
+
+Loading::Loading()
+{
+}
+
+Loading::~Loading()
+{
+
+}
+
+void Loading::Initialize(DirectXCommon* dxCommon, Input* input, Audio* audio)
+{
+	// Checking for nullptr
+	assert(dxCommon);
+	assert(input);
+	assert(audio);
+
+	// Assigning variables to this
+	this->dxCommon = dxCommon;
+	this->input = input;
+	this->audio = audio;
+
+	// Camera initialization
+	camera = new DebugCamera(WinApp::window_width, WinApp::window_height, input);
+
+	// Collision Manager initialization
+	collisionManager = CollisionManager::GetInstance();
+
+	// Particle Manager initialization/generation
+	particleMan = ParticleManager::GetInstance();
+	particleMan->SetCamera(camera);
+
+	// Light Group Creation
+	lightGroup = LightGroup::Create();
+
+	// Setting DxCommon device
+	FBXGeneration::SetDevice(dxCommon->GetDevice());
+	EnemyHuman::SetDevice(dxCommon->GetDevice());
+	Player::SetDevice(dxCommon->GetDevice());
+
+	// Setting camera
+	Object3d::SetCamera(camera);
+	FBXGeneration::SetCamera(camera);
+	EnemyHuman::SetCamera(camera);
+	Player::SetCamera(camera);
+	PlayerPositionObject::SetCamera(camera);
+
+	// Loading debug text
+	if (!Sprite::LoadTexture(debugTextTexNumber, "debugfont.png")) { assert(0); return; }
+
+	// Debug text initialization
+	debugText = DebugText::GetInstance();
+	debugText->Initialize(debugTextTexNumber);
+}
+
+void Loading::Update()
+{
+
+}
+
+void Loading::Draw()
+{
+	// Setting command list
+	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
+
+#pragma region backgroundSpriteDrawing
+	// Background sprite drawing preprocessing
+	//Sprite::PreDraw(cmdList);
+	// Background sprite drawing
+	//spriteBG->Draw();
+
+	/// <summary>
+	/// Background sprite drawing processes
+	/// </summary>
+
+	// Sprite drawing post-processing
+	//Sprite::PostDraw();
+	// Clearing depth buffer
+	//dxCommon->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3DDrawing
+	Object3d::PreDraw(cmdList);
+
+	// 3D Object Drawing
+
+	// Particle drawing
+	particleMan->Draw(cmdList);
+
+	Object3d::PostDraw();
+#pragma endregion
+
+#pragma region foregroundSpriteDrawing
+	Sprite::PreDraw(cmdList);
+
+	// Foreground sprite drawing
+
+	// Debug text drawing
+	debugText->DrawAll(cmdList);
+
+	// Sprite post draw
+	Sprite::PostDraw();
+#pragma endregion
+}
