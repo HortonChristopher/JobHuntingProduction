c7e3f2e85b6def9a8767bd8fe5ce46b5576b31b5
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2023-01-16 23:58:26 +0900

    ShadowMap Values added

--- Pursuer/tools/ShadowMap.cpp
+++ Pursuer/tools/ShadowMap.cpp
@@ -1,1 +1,51 @@
 #include "ShadowMap.h"
+
+ShadowMap::ShadowMap()
+{
+	HRESULT result;
+	dev = DirectXCommon::GetInstance()->GetDevice();
+	cmdList = DirectXCommon::GetInstance()->GetCommandList();
+
+	vertices[0].uv = { 0.0f,1.0f };
+	vertices[1].uv = { 0.0f,0.0f };
+	vertices[2].uv = { 1.0f,1.0f };
+	vertices[3].uv = { 1.0f,0.0f };
+	vertices[0].pos = { -1.0f,-1.0f,0.0f };
+	vertices[1].pos = { -1.0f,+1.0f,0.0f };
+	vertices[2].pos = { +1.0f,-1.0f,0.0f };
+	vertices[3].pos = { +1.0f,+1.0f,0.0f };
+
+	// Vertex Buffer Generation
+	result = dev->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(sizeof(vertices)),
+		D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertBuff)
+	);
+	assert(SUCCEEDED(result));
+
+	// Transfer Vertex Buffer
+	Vertex* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	if (SUCCEEDED(result))
+	{
+		memcpy(vertMap, vertices.data(), sizeof(vertices));
+		vertBuff->Unmap(0, nullptr);
+	}
+
+	// Create Vertex Buffer View
+	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
+	vbView.SizeInBytes = sizeof(vertices);
+	vbView.StrideInBytes = sizeof(vertices[0]);
+
+	// Create Constant Buffer
+	result = dev->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer((sizeof(ConstBufferDate) + 0xff) & ~0xff),
+		D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
+		IID_PPV_ARGS(&constBuff));
+	assert(SUCCEEDED(result));
+
+	Initialize();
+}
