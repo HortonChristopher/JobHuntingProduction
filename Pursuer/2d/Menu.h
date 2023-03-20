925b6f7d9efef8923e527b98a71c241fd6e353dc
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2023-03-20 22:55:41 +0900

    Added header file private variables

--- Pursuer/2d/Menu.h
+++ Pursuer/2d/Menu.h
@@ -1,4 +1,12 @@
 #pragma once
+#include <cstdlib>
+#include <memory>
+#include <iostream>
+
+#include "Sprite.h"
+#include "Vector.h"
+
+using namespace std;
 
 class Menu
 {
@@ -10,5 +18,75 @@ public:
 	void Draw();
 
 private:
+	void BaseMenu();
+	void Setting();
+	void SensitiveSetting();
+	void OperateSetting();
+	void ScreenSettingProcess();
+	void EndCheckProcess();
 
+	enum MenuState
+	{
+		Base,
+		SettingSelect,
+		PadSensitiveSetting,
+		PadOperateSetting,
+		ScreenSetting,
+		EndCheck,
+	};
+	MenuState menuState;
+	MenuState nextMenuState;
+
+	int openCounter = 0;
+	int closeCounter = 0;
+	bool startClose = false;
+
+	std::unique_ptr<Sprite> menuUI;
+	std::unique_ptr<Sprite> backTex;
+	std::unique_ptr<Sprite> restart;
+	std::unique_ptr<Sprite> end;
+	std::unique_ptr<Sprite> setting;
+	std::unique_ptr<Sprite> yes;
+	std::unique_ptr<Sprite> no;
+	std::unique_ptr<Sprite> endCheck;
+	std::unique_ptr<Sprite> restartCheck;
+	std::unique_ptr<Sprite> sensitiveSetting;
+	std::unique_ptr<Sprite> operateSetting;
+	std::unique_ptr<Sprite> sensitiveSettingBar;
+	std::unique_ptr<Sprite> dot;
+	std::unique_ptr<Sprite> check;
+	std::unique_ptr<Sprite> check2;
+	std::unique_ptr<Sprite> jump;
+	std::unique_ptr<Sprite> cameraReset;
+	std::unique_ptr<Sprite> action;
+	std::unique_ptr<Sprite> upsideDown;
+	std::unique_ptr<Sprite> changeRun;
+	std::unique_ptr<Sprite> flipLeftRight;
+	std::unique_ptr<Sprite> jumpOperate;
+	std::unique_ptr<Sprite> cameraResetOperate;
+	std::unique_ptr<Sprite> actionOperate;
+	std::unique_ptr<Sprite> changeRunOperate;
+	std::unique_ptr<Sprite> selectFrame;
+	std::unique_ptr<Sprite> viewCollision;
+	std::unique_ptr<Sprite> screenSetting;
+
+	bool operateChangeFlag = false;
+
+	int select = 0;
+	float easingScale;
+	Vector2 menuUIScale;
+	Vector2 backTexScale;
+	Vector2 restartScale;
+	Vector2 endScale;
+	Vector2 settingScale;
+	Vector2 sensitiveSettingScale;
+	Vector2	operateSettingScale;
+	Vector2 yesScale;
+	Vector2 noScale;
+	Vector2 dotPos;
+	Vector2 selectFramePos;
+	Vector2 screenSettingScale;
+	bool close = false;
+
+	bool returnTitle = false;
 };
