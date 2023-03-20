90eea4c4f9c70a3ebdef92a49f2c1a33cb285e5f
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2023-03-20 22:08:14 +0900

    Menu class files added

---
+++ Pursuer/2d/Menu.cpp
@@ -0,0 +1,26 @@
+#include "Menu.h"
+
+Menu::Menu()
+{
+
+}
+
+Menu::~Menu()
+{
+
+}
+
+void Menu::Initialize()
+{
+
+}
+
+bool Menu::Update()
+{
+
+}
+
+void Menu::Draw()
+{
+
+}
---
+++ Pursuer/2d/Menu.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Menu
+{
+public:
+	Menu();
+	~Menu();
+	void Initialize();
+	bool Update();
+	void Draw();
+
+private:
+
+};
